04deb76b597a44bbcab28a60fd061f9b3d23642a
Anmol Malik <68939602+theanmolmalik@users.noreply.github.com>
2022-07-02 03:07:19 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home()
+{
+    return (
+        <div>
+            <img style={{maxWidth:"100%",height:"auto"}} src="https://www.freecodecamp.org/news/content/images/size/w2000/2022/03/pexels-rodnae-productions-7581108.jpg" alt="main"/>
+            <h2>Tohyo, An Online Voting Platform</h2>
+        </div>
+    );
+}
